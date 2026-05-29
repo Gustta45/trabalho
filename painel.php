64a1be186465380afa04031c07e28b51c90ec538
Gustta45 <gustavoselau.nh040@academico.ifsul.edu.br>
2026-05-29 08:29:39 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -1,1 +1,35 @@
 
+<?php
+session_start();
+
+if(!isset($_SESSION['usuario'])){
+    header("Location: index.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Painel</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h2>Bem-vindo</h2>
+
+    <p>
+        Usuário logado:
+        <strong>
+            <?php echo $_SESSION['usuario']; ?>
+        </strong>
+    </p>
+
+    <a href="logout.php">
+        <button>Sair</button>
+    </a>
+</div>
+
+</body>
+</html>
