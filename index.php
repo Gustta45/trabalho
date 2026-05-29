9669dc7ada983b10d049aecef7e619dc34b67a9e
Gustta45 <gustavoselau.nh040@academico.ifsul.edu.br>
2026-05-29 08:27:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
+<div class="container">
+    <h2>Login</h2>
 
+    <form action="login.php" method="POST">
+        <input type="text" name="usuario" placeholder="Usuário" required>
 
-?>
+        <input type="password" name="senha" placeholder="Senha" required>
+
+        <button type="submit">Entrar</button>
+    </form>
+</div>
+
+</body>
+</html>
